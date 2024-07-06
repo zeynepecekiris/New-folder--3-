538f29920c1a9631c03ad0182aee85d41113ba6f
zeynepecekiris <zeynep.ece933@gmail.com>
2024-07-06 16:31:16 +0300

    first commit

---
+++ can.js
@@ -0,0 +1,36 @@
+// console.log(true,false,"true","false");
+
+let email="canboz@gmail.com";
+
+
+
+let varmi=email.includes("g");
+let varmi2=adSoyad.includes("tuce");
+// console.log(varmi2);
+
+
+// console.log(yas==29);
+
+// console.log(yas==35);
+
+let ad="can";
+// console.log(ad=="can");
+
+
+let yas ="29";
+// console.log("yas"+1);
+
+// FOR 
+
+for(let i = 0; i <10; i++)
+{
+    console.log(i);
+}
+console.log("dongu bitti");
+
+const adSoyad=["can","tuce","elif"];
+
+for(let i = 0; i < adSoyad.length; i++)
+{
+    console.log(adSoyad[i]);
+}
